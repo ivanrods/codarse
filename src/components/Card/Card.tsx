e460a479783ee1b13e68a9ae84716f8c5b278456
Ivan <contaivanrodrigues@gmail.com>
2025-04-29 21:33:04 -0300

    feat: improve card and section components

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,26 +1,33 @@
-import Image from "next/image";
-import Link from "next/link";
+import Image from 'next/image';
+import Link from 'next/link';
 
-interface ICardProps {
+
+export interface ICardProps {
   href: string;
   image: string;
   title: string;
   description: string;
 }
-export const Card = ({ href, image, title, description }: ICardProps) => {
+export const Card = ({ title, description, image, href }: ICardProps) => {
   return (
-    <Link href={href} className="">
-      <article className="flex flex-col gap-2 p-2 rounded hover:no-underline sm:hover:bg-teal-600">
+    <Link href={href} className='hover:no-underline'>
+      <article className='flex gap-2 flex-col p-2 rounded sm:hover:bg-primary'>
         <Image
-          src={image}
-          width={1000}
           height={0}
+          src={image}
           alt={title}
+          width={1000}
           draggable={false}
-          className="aspect-video object-cover rounded-2xl"
+          className='aspect-video object-cover rounded-2xl'
         />
-        <h4 className="font-extrabold text-lg">{title}</h4>
-        <p className="line-clamp-3">{description}</p>
+
+        <h4 className='font-extrabold text-lg'>
+          {title}
+        </h4>
+
+        <p className='line-clamp-3 sm:line-clamp-4 md:line-clamp-5'>
+          {description}
+        </p>
       </article>
     </Link>
   );
--- src/components/section/Section.tsx
+++ src/components/section/Section.tsx
@@ -1,115 +1,32 @@
-import { Card } from "../Card/Card";
+import { Card, ICardProps } from "../Card/Card";
 interface ISectionProps {
   title: string;
-  variant: "grid" | "h-list";
+  items: ICardProps[];
+  variant: 'grid' | 'h-list';
 }
-export const Section = ({ title, variant = "grid" }: ISectionProps) => {
+export const Section = ({ title, items, variant = 'grid' }: ISectionProps) => {
   return (
-    <section className="flex flex-col gap-4 px-4">
-      <h2 className="font-bold text-xl ">{title}</h2>
+    <section className='flex flex-col gap-4 px-4'>
+      <h2 className='font-bold text-xl'>
+        {title}
+      </h2>
+
       <ul
         data-variant={variant}
         className='grid gap-2 grid-cols-1 sm:grid-cols-none data-[variant=grid]:sm:grid-cols-2 data-[variant=grid]:md:grid-cols-3 data-[variant=h-list]:sm:grid-flow-col data-[variant=h-list]:sm:overflow-x-auto'
       >
-        <li data-variant={variant} className='w-full data-[variant=h-list]:sm:w-72'>
-          <Card
-            href="/cursos/123"
-            image="https://i.ytimg.com/vi/SVepTuBK4V0/hqdefault.jpg"
-            title="Curso de API rest, node e Typescript"
-            description={`A medida que um sistema começa a ganhar forma, para garantirmos que
-          apenas pessoas autorizadas possam acessar os recursos da aplicação
-          precisamos de alguma forma garantir a autenticidade do usuário que
-          está utilizando o sistema. Nessa vamos começar o desenvolvimento do
-          controle de usuário para o nosso sistema, vamos desenvolver as
-          migration para a entidade que deve armazenar os usuário que podem
-          acessar o sistema. Links mencionados no vídeo: 📖 Repositório do
-          projeto - https://github.com/lvsouza/youtube-cu... 🗨️ Discord - /
-          discord Outros cursos no canal: 🎩 Figma para DEVs - • Figma para
-          DEVs: #00 - Apresentação d... ☪️ Curso de estilização no React - •
-          Estilização no React: Aula 00 - Curso... ⭐️ Curso de React com
-          typescript - • Curso de react: Aula 01 - Começando c... 👑 React,
-          Material UI 5 e Typescript - • React, Material UI 5 e Typescript:
-          #0... 🏆 Curso de API Rest, Node e Typescript - • API Rest, Node e
-          Typescript: #00 - Ap... Livros recomendados: 📘 Código limpo -
-          https://s.shopee.com.br/1LKnAYYXtW 📘 Arquitetura limpa -
-          https://s.shopee.com.br/4fbF8sGs4H 📘 Migrando sistemas monolíticos -
-          https://s.shopee.com.br/606cjP12q9 Conteúdo: 0:00 | Introdução 0:21 |
-          O que faremos nessa aula 5:47 | Revisando modelagem para controle de
-          usuários 8:00 | Criando model para controle de usuários 9:48 | Criando
-          migration para tabela de usuários 15:05 | Rodando migration para
-          localhost 16:40 | Alterando migration e dica importante 19:20 |
-          Considerações finais CODARSE - Criando o controle de usuários
-          #typescript #cursotypescript #javascript #cursodejavascript
-          #reactetypescript #learningtypescript #learningjavascript #apirest
-          #apinode`}
-          />
-        </li>
-        <li data-variant={variant} className='w-full data-[variant=h-list]:sm:w-72'>
-          <Card
-            href="/cursos/123"
-            image="https://i.ytimg.com/vi/SVepTuBK4V0/hqdefault.jpg"
-            title="Curso de API rest, node e Typescript"
-            description={`A medida que um sistema começa a ganhar forma, para garantirmos que
-          apenas pessoas autorizadas possam acessar os recursos da aplicação
-          precisamos de alguma forma garantir a autenticidade do usuário que
-          está utilizando o sistema. Nessa vamos começar o desenvolvimento do
-          controle de usuário para o nosso sistema, vamos desenvolver as
-          migration para a entidade que deve armazenar os usuário que podem
-          acessar o sistema. Links mencionados no vídeo: 📖 Repositório do
-          projeto - https://github.com/lvsouza/youtube-cu... 🗨️ Discord - /
-          discord Outros cursos no canal: 🎩 Figma para DEVs - • Figma para
-          DEVs: #00 - Apresentação d... ☪️ Curso de estilização no React - •
-          Estilização no React: Aula 00 - Curso... ⭐️ Curso de React com
-          typescript - • Curso de react: Aula 01 - Começando c... 👑 React,
-          Material UI 5 e Typescript - • React, Material UI 5 e Typescript:
-          #0... 🏆 Curso de API Rest, Node e Typescript - • API Rest, Node e
-          Typescript: #00 - Ap... Livros recomendados: 📘 Código limpo -
-          https://s.shopee.com.br/1LKnAYYXtW 📘 Arquitetura limpa -
-          https://s.shopee.com.br/4fbF8sGs4H 📘 Migrando sistemas monolíticos -
-          https://s.shopee.com.br/606cjP12q9 Conteúdo: 0:00 | Introdução 0:21 |
-          O que faremos nessa aula 5:47 | Revisando modelagem para controle de
-          usuários 8:00 | Criando model para controle de usuários 9:48 | Criando
-          migration para tabela de usuários 15:05 | Rodando migration para
-          localhost 16:40 | Alterando migration e dica importante 19:20 |
-          Considerações finais CODARSE - Criando o controle de usuários
-          #typescript #cursotypescript #javascript #cursodejavascript
-          #reactetypescript #learningtypescript #learningjavascript #apirest
-          #apinode`}
-          />
-        </li>
-        <li data-variant={variant} className='w-full data-[variant=h-list]:sm:w-72'>
-          <Card
-            href="/cursos/123"
-            image="https://i.ytimg.com/vi/SVepTuBK4V0/hqdefault.jpg"
-            title="Curso de API rest, node e Typescript"
-            description={`A medida que um sistema começa a ganhar forma, para garantirmos que
-          apenas pessoas autorizadas possam acessar os recursos da aplicação
-          precisamos de alguma forma garantir a autenticidade do usuário que
-          está utilizando o sistema. Nessa vamos começar o desenvolvimento do
-          controle de usuário para o nosso sistema, vamos desenvolver as
-          migration para a entidade que deve armazenar os usuário que podem
-          acessar o sistema. Links mencionados no vídeo: 📖 Repositório do
-          projeto - https://github.com/lvsouza/youtube-cu... 🗨️ Discord - /
-          discord Outros cursos no canal: 🎩 Figma para DEVs - • Figma para
-          DEVs: #00 - Apresentação d... ☪️ Curso de estilização no React - •
-          Estilização no React: Aula 00 - Curso... ⭐️ Curso de React com
-          typescript - • Curso de react: Aula 01 - Começando c... 👑 React,
-          Material UI 5 e Typescript - • React, Material UI 5 e Typescript:
-          #0... 🏆 Curso de API Rest, Node e Typescript - • API Rest, Node e
-          Typescript: #00 - Ap... Livros recomendados: 📘 Código limpo -
-          https://s.shopee.com.br/1LKnAYYXtW 📘 Arquitetura limpa -
-          https://s.shopee.com.br/4fbF8sGs4H 📘 Migrando sistemas monolíticos -
-          https://s.shopee.com.br/606cjP12q9 Conteúdo: 0:00 | Introdução 0:21 |
-          O que faremos nessa aula 5:47 | Revisando modelagem para controle de
-          usuários 8:00 | Criando model para controle de usuários 9:48 | Criando
-          migration para tabela de usuários 15:05 | Rodando migration para
-          localhost 16:40 | Alterando migration e dica importante 19:20 |
-          Considerações finais CODARSE - Criando o controle de usuários
-          #typescript #cursotypescript #javascript #cursodejavascript
-          #reactetypescript #learningtypescript #learningjavascript #apirest
-          #apinode`}
-          />
-        </li>
+
+        {items.map(item => (
+          <li key={item.title} data-variant={variant} className='w-full data-[variant=h-list]:sm:w-72'>
+            <Card
+              href={item.href}
+              title={item.title}
+              image={item.image}
+              description={item.description}
+            />
+          </li>
+        ))}
+
       </ul>
     </section>
   );
