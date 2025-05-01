15c11f029dd436224a300f39aace6a1464334881
Ivan <contaivanrodrigues@gmail.com>
2025-05-01 13:40:51 -0300

    feat: improve header and global css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
 import "./globals.css";
-import { Header } from "@/components/Header/Header";
+import { Header } from "@/components/header/Header";
 
 const nunito = Nunito({
   variable: "--font-geist-sans",
