963bc4dbaa41a94056a256f594c425980c8f555f
Ivan <contaivanrodrigues@gmail.com>
2025-05-04 14:43:48 -0300

    feat: add component copy content

--- src/components/course-header/CourseHeader.tsx
+++ src/components/course-header/CourseHeader.tsx
@@ -1,4 +1,6 @@
+import { MdShare } from "react-icons/md";
 import { CollapsinbleText } from "./components/CollapsinbleText";
+import { ContentCopy } from "./components/CopyContext";
 
 export const CourseHeader = () => {
   return (
@@ -16,7 +18,12 @@ export const CourseHeader = () => {
         gratuitamente. #CODARSE
       </CollapsinbleText>
       <div className="flex gap-2 items-center">
-        <button className="py-2 px-4 bg-stone-800 rounded-full">Compartilhar</button>
+        <ContentCopy title="Copie o conteúdo a baixo" content="https://www.youtube.com/watch?">
+          <button className="flex gap-2 items-center py-2 px-4 bg-stone-800 rounded-full">
+            <MdShare />
+            Compartilhar
+          </button>
+        </ContentCopy>
         <span>59 aulas</span>
       </div>
     </div>
