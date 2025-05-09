83aacae5da0b054650647833c8273e3dda5403c8
Ivan <contaivanrodrigues@gmail.com>
2025-05-09 14:30:49 -0300

    feat: add player header

--- src/app/player/[courseId]/[classId]/page.tsx
+++ src/app/player/[courseId]/[classId]/page.tsx
@@ -1,13 +1,23 @@
+import { PlayerHeader } from "@/components/Player";
 
 interface Props {
-    params: {
-        classId: string;
-        courseId: string;
-    }
+  params: {
+    classId: string;
+    courseId: string;
+  };
 }
 
-export default function PagePlayer({params:{classId, courseId}}:Props){
-    return(
-        <>{classId} {courseId}</>
-    )
+export default function PagePlayer({ params: { classId, courseId } }: Props) {
+  return (
+    <>
+      <PlayerHeader
+        title="NextJS, TailwindCSS e Typescript: #32 - Criando componente de header
+          para a tela de player"
+        subtitle="🔔 NextJS, TailwindCSS e Typescript"
+      />
+      <>
+        {classId} {courseId}
+      </>
+    </>
+  );
 }
