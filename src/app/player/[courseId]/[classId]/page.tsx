29188c1016ffd159c4d0c73bd7a7a5f7135e8adc
Ivan <contaivanrodrigues@gmail.com>
2025-05-18 13:55:15 -0300

    feat: add play next button

--- src/app/player/[courseId]/[classId]/page.tsx
+++ src/app/player/[courseId]/[classId]/page.tsx
@@ -1,4 +1,4 @@
-
+'use client'
 import {
   PlayerHeader,
   PlayerPlaylist,
@@ -22,66 +22,78 @@ export default function PagePlayer({ params: { classId, courseId } }: Props) {
         subtitle="🔔 NextJS, TailwindCSS e Typescript"
       />
       <div className="flex gap-2 h-[calc(100vh-72px)]">
-      <div className='max-w-96 '>
+        <div className="max-w-96 ">
           <PlayerPlaylist
-          playingClassId={classId}
-          playingCourseId={courseId}
+            playingClassId={classId}
+            playingCourseId={courseId}
             classGroups={[
               {
-                title: 'Introdução e apresentação do projeto',
+                title: "Introdução e apresentação do projeto",
                 classes: [
                   {
                     done: true,
-                    classId: 'aula-00',
-                    title: '0 API Rest, Node e Typescript: #00 - Apresentação do curso, tecnologias usadas e muito mais'
+                    classId: "aula-00",
+                    title:
+                      "0 API Rest, Node e Typescript: #00 - Apresentação do curso, tecnologias usadas e muito mais",
                   },
                   {
                     done: false,
-                    classId: 'aula-01',
-                    title: '01 API Rest, Node e Typescript: #01 - Apresentação do curso, tecnologias usadas e muito mais'
+                    classId: "aula-01",
+                    title:
+                      "01 API Rest, Node e Typescript: #01 - Apresentação do curso, tecnologias usadas e muito mais",
                   },
                   {
                     done: false,
-                    classId: 'aula-02',
-                    title: '2 API Rest, Node e Typescript: #02 - Apresentação do curso, tecnologias usadas e muito mais'
+                    classId: "aula-02",
+                    title:
+                      "2 API Rest, Node e Typescript: #02 - Apresentação do curso, tecnologias usadas e muito mais",
                   },
                   {
                     done: false,
-                    classId: 'aula-03',
-                    title: '3 API Rest, Node e Typescript: #03 - Apresentação do curso, tecnologias usadas e muito mais'
+                    classId: "aula-03",
+                    title:
+                      "3 API Rest, Node e Typescript: #03 - Apresentação do curso, tecnologias usadas e muito mais",
                   },
-                ]
+                ],
               },
               {
-                title: 'Introdução e apresentação do projeto',
+                title: "Introdução e apresentação do projeto",
                 classes: [
                   {
                     done: true,
-                    classId: 'aula-04',
-                    title: '4 API Rest, Node e Typescript: #04 - Apresentação do curso, tecnologias usadas e muito mais'
+                    classId: "aula-04",
+                    title:
+                      "4 API Rest, Node e Typescript: #04 - Apresentação do curso, tecnologias usadas e muito mais",
                   },
                   {
                     done: false,
-                    classId: 'aula-05',
-                    title: '5 API Rest, Node e Typescript: #05 - Apresentação do curso, tecnologias usadas e muito mais'
+                    classId: "aula-05",
+                    title:
+                      "5 API Rest, Node e Typescript: #05 - Apresentação do curso, tecnologias usadas e muito mais",
                   },
                   {
                     done: false,
-                    classId: 'aula-06',
-                    title: '6 API Rest, Node e Typescript: #06 - Apresentação do curso, tecnologias usadas e muito mais'
+                    classId: "aula-06",
+                    title:
+                      "6 API Rest, Node e Typescript: #06 - Apresentação do curso, tecnologias usadas e muito mais",
                   },
                   {
                     done: false,
-                    classId: 'aula-07',
-                    title: '7 API Rest, Node e Typescript: #07 - Apresentação do curso, tecnologias usadas e muito mais'
+                    classId: "aula-07",
+                    title:
+                      "7 API Rest, Node e Typescript: #07 - Apresentação do curso, tecnologias usadas e muito mais",
                   },
-                ]
-              }
+                ],
+              },
             ]}
           />
         </div>
 
-        <div className="flex flex-1"><PlayerVideoPlayer videoId="Bj1OKr_UKxM"/></div>
+        <div className="flex-1 ">
+          <div className="aspect-video">
+            <PlayerVideoPlayer videoId="Bj1OKr_UKxM" onPlayNext={() => console.log('play')}/>
+          </div>
+        </div>
       </div>
     </main>
   );
