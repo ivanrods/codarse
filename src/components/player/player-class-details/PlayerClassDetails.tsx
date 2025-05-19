5157e35ad7706a415728ab0944b5c497c4c85ecb
Ivan <contaivanrodrigues@gmail.com>
2025-05-19 13:56:53 -0300

    feat: add tabs components

--- src/components/player/player-class-details/PlayerClassDetails.tsx
+++ src/components/player/player-class-details/PlayerClassDetails.tsx
@@ -4,6 +4,7 @@ import { useRouter } from "next/navigation";
 import { PlayerGrupClassProps } from "../playlist/components/PlayerGrupClass";
 import { PlayerVideoPlayer } from "./components/PlayerVideoPlayer";
 import { useMemo } from "react";
+import * as Tabs from "@radix-ui/react-tabs";
 
 interface IPlayerClassDetailsProps {
   playingClassId: string;
@@ -46,7 +47,34 @@ export const PlayerClassDetails = ({
         />
       </div>
 
-      <div>Descrição</div>
+      <Tabs.Root defaultValue="class-details">
+        <Tabs.TabsList className="flex gap-4 ">
+          <Tabs.Trigger
+            value="class-details"
+            className="p-2 flex items-center justify-center border-transparent data-[state=active]:border-b-teal-600 border-b-4"
+          >
+            Visao geral
+          </Tabs.Trigger>
+
+          <Tabs.Trigger
+            value="class-comments"
+            className="p-2 flex items-center justify-center border-transparent data-[state=active]:border-b-teal-600 border-b-4"
+          >
+            Comentarios
+          </Tabs.Trigger>
+
+          <Tabs.Trigger
+            value="course-details"
+            className="p-2 flex items-center justify-center border-transparent data-[state=active]:border-b-teal-600 border-b-4"
+          >
+            Visao geral do curso
+          </Tabs.Trigger>
+        </Tabs.TabsList>
+        <hr className="border- border-neutral-500" />
+        <Tabs.Content value="class-details">Detalhes da aula</Tabs.Content>
+        <Tabs.Content value="class-comments">Comentarios da aula</Tabs.Content>
+        <Tabs.Content value="course-details">Detalhes curso</Tabs.Content>
+      </Tabs.Root>
     </div>
   );
 };
