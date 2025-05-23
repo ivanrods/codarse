b19c0e74385d04f6894836069338883365b7f5d6
Ivan <contaivanrodrigues@gmail.com>
2025-05-23 14:20:45 -0300

    feat: add cache for youtube calls

--- src/shared/services/api-youtube/index.ts
+++ src/shared/services/api-youtube/index.ts
@@ -1,18 +1,34 @@
 import { youtube } from "@googleapis/youtube";
 
+const fetchWithNextConfig = (
+  nextConfig?: NextFetchRequestConfig
+): typeof fetch => {
+  return (input, params = {}) => {
+    return fetch(input, { ...params, next: nextConfig });
+  };
+};
+
 const YouTubeAPIClient = youtube({
   version: "v3",
   auth: process.env.YOUTUBE_API_KEY,
+  fetchImplementation: fetchWithNextConfig(),
 });
 
 export const APIYouTube = {
   course: {
     getAll: async () => {
-      const { data } = await YouTubeAPIClient.playlists.list({
-        maxResults: 50,
-        part: ["snippet"],
-        channelId: "UCtJ3j1ANBe91ZHXzw6FJveg",
-      });
+      const { data } = await YouTubeAPIClient.playlists.list(
+        {
+          maxResults: 50,
+          part: ["snippet"],
+          channelId: "UCtJ3j1ANBe91ZHXzw6FJveg",
+        },
+        {
+          fetchImplementation: fetchWithNextConfig({
+            revalidate: 60 * 60 * 48,
+          }),
+        }
+      );
 
       const courses = (data.items || []).map((playlistItem) => ({
         id: playlistItem.id || "",
