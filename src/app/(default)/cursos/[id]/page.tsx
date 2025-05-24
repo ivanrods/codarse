cd55a7592dff4ec8c8d76bb6538bb3a38f5e47b0
Ivan <contaivanrodrigues@gmail.com>
2025-05-24 14:20:51 -0300

    feat: add get course by id

--- src/app/(default)/cursos/[id]/page.tsx
+++ src/app/(default)/cursos/[id]/page.tsx
@@ -1,76 +1,67 @@
+import { Metadata } from "next";
+
 import { CourseContent } from "@/components/course-content/CourseContent";
 import { CourseHeader } from "@/components/course-header/CourseHeader";
+import { APIYouTube } from "@/shared/services/api-youtube";
 import { StartCourse } from "@/components/StartCourse";
-import { Metadata } from "next";
 
 interface Props {
   params: { id: string };
 }
 
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const courseDetail = await APIYouTube.course.getById(params.id);
+
   return {
-    title: params.id,
-    description: params.id,
+    title: courseDetail.title,
+    description: courseDetail.description,
+    openGraph: {
+      locale: "pt_BR",
+      type: "video.other",
+      title: courseDetail.title,
+      images: courseDetail.image,
+      description: courseDetail.description,
+      videos: courseDetail.classGroups.reduce<string[]>(
+        (previous, current) => [
+          ...previous,
+          ...current.classes.map(
+            (classItem) =>
+              `https://codarse.com/player/${current.courseId}/${classItem.id}`
+          ),
+        ],
+        []
+      ),
+    },
   };
 }
-export default function PageCurseDetail({ params }: Props) {
+
+export default async function PageCourseDetail({ params }: Props) {
+  const courseDetail = await APIYouTube.course.getById(params.id);
+
+  const firstClass = courseDetail.classGroups.at(0)?.classes.at(0);
+
   return (
     <main className="mt-8 flex justify-center">
-      <div className="w-full min-[880px]:max-w-[880px] lg:px-0 px-2 flex flex-col md:flex-row-reverse gap-4 not-first:">
-        <div className="flex-1">
-          <StartCourse
-            title="Curso de figam para devs"
-            idClass="1"
-            idCourse="1"
-            imageUrl="https://i.ytimg.com/vi/SVepTuBK4V0/hqdefault.jpg"
+      <div className="w-full min-[880px]:max-w-[880px] px-2 flex flex-col gap-4 lg:px-0 md:flex-row-reverse">
+        {firstClass && (
+          <div className="flex-1">
+            <StartCourse
+              idClass={firstClass.id}
+              title={firstClass.title}
+              idCourse={courseDetail.id}
+              imageUrl={courseDetail.image}
+            />
+          </div>
+        )}
+
+        <div className="flex-[2] flex flex-col gap-12 pb-12">
+          <CourseHeader
+            title={courseDetail.title}
+            description={courseDetail.description}
+            numberOfClasses={courseDetail.numberOfClasses}
           />
-        </div>
-        <div className="flex-2 flex flex-col gap-12 pb-12">
-          <CourseHeader title="Curso de Figma para DEVs" description="Os melhores desenvolvedores do mercado fazem questão que estar
-                  preparados para os mais diversos tipos de desafios nas suas carreiras. A
-                  habilidade de desenvolver protótipos ou mesmo de entender como um
-                  protótipo foi desenvolvido pode ser um baita diferencial para você.
-                  Nesse curso que te mostrar de forma simples e prática como desenvolver
-                  protótipos no figma, vamos aproveitar certos conhecimentos de
-                  programação ao decorrer do curso. Tenho certeza que esse tem o potencial
-                  de ser o melhor curso de figma para desenvolvedores disponíveis
-                  gratuitamente. #CODARSE" numberOfClasses={48}/>
-          <CourseContent
-            classGrups={[
-              {
-                courseId: "123",
-                title: "Intrudução",
-                classes: [
-                  {
-                    id: "123",
-                    title:
-                      "NextJS, TailwindCSS e Typescript: #28 - Criando componente de Conteúdo de curso",
-                  },
-                  {
-                    id: "123",
-                    title:
-                      "NextJS, TailwindCSS e Typescript: #28 - Criando componente de Conteúdo de curso",
-                  },
-                ],
-              },
-              {
-                courseId: "123",
-                title: "Primeiras configurações",
-                classes: [
-                  {
-                    id: "123",
-                    title:
-                      "NextJS, TailwindCSS e Typescript: #28 - Criando componente de Conteúdo de curso",
-                  },
-                  {
-                    id: "123",
-                    title:
-                      "NextJS, TailwindCSS e Typescript: #28 - Criando componente de Conteúdo de curso",
-                  },
-                ],
-              },
-            ]}
-          />
+
+          <CourseContent classGrups={courseDetail.classGroups} />
         </div>
       </div>
     </main>
